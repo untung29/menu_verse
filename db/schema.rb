0c5623a0be6ce1b56cf44913f16c93bca2489b3a
Untung <u.untung0029@gmail.com>
2024-11-19 19:25:46 +0800

    Section Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_19_112139) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_19_112402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_19_112139) do
     t.string "state", null: false
     t.date "start_date", null: false
     t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "label", null: false
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
