048f0b9c1ec11dea8678d88497662181c80fc640
Untung <u.untung0029@gmail.com>
2024-11-21 16:33:31 +0800

    Remove test code

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,11 +3,5 @@
 module Types
   class MutationType < Types::BaseObject
     field :update_menu, mutation: Mutations::UpdateMenu
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
