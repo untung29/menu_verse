3c13e05b3d75835a9ac5c3ac3347b16057f8a081
Untung <u.untung0029@gmail.com>
2024-11-21 15:52:59 +0800

    Fetch specific menu

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,10 +2,19 @@
 
 module Types
   class QueryType < Types::BaseObject
+    # Fetch all menus
     field :menus, [ MenuType ], null: false, description: "Retrieve a list of menus"
 
     def menus
       Menu.all
+    end
+
+    # Fetch specific menu
+    field :menu, MenuType, null: false, description: "Retrieve a menu by its ID" do
+       argument :id, ID, required: true
+    end
+    def menu(id:)
+      Menu.find(id)
     end
   end
 end
