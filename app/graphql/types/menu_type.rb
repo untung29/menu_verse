77648e5db1e83c190e338226a37813816e0be893
Untung <u.untung0029@gmail.com>
2024-11-21 15:43:12 +0800

    Query for Menu model

--- app/graphql/types/menu_type.rb
+++ app/graphql/types/menu_type.rb
@@ -1,6 +1,10 @@
 module Types
     class MenuType < Types::BaseObject
-        field :id, ID, null:false
+        field :id, ID, null: false
         field :label, String, null: false
+        field :state, String, null: false
+        # TODO: update this later to have a better type
+        field :start_date, GraphQL::Types::ISO8601Date, null: false
+        field :end_date, GraphQL::Types::ISO8601Date, null: true
     end
 end
