6fc7907e23a7638d4c037c63dcbd6578ff88c06c
Untung <u.untung0029@gmail.com>
2024-11-21 22:05:40 +0800

    Update Item

---
+++ app/graphql/mutations/update_item.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Mutations
+  class UpdateItem < BaseMutation
+    # Return Fields
+    field :item, Types::ItemType, null: true
+    field :errors, [ String ], null: false
+
+    # Arguments
+    argument :id, ID, required: true
+    argument :label, String, required: false
+    argument :description, String, required: false
+    argument :price, Float, required: false
+
+    def resolve(id:, label: nil, description: nil, price: nil)
+      item = Item.find_by(id: id)
+      # Return error if menu not found
+      { item: nil, errors: [ "Menu not found" ] } unless item
+
+
+      if item.update({ label: label, description: description, price: price }.compact)
+        { item: item, errors: [] }
+      else
+        { item: nil, errors: item.errors.full_messages }
+      end
+    end
+  end
+end
