b8c6dab206041bbd90c2f8ad79e54d93697088fe
Untung <u.untung0029@gmail.com>
2024-11-22 16:10:27 +0800

    selection_required_min validation

--- app/models/modifier_group.rb
+++ app/models/modifier_group.rb
@@ -3,4 +3,6 @@ class ModifierGroup < ApplicationRecord
     has_one :item, through: :item_modifier_group
 
     has_many :modifiers
+
+    validates :selection_required_min, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
