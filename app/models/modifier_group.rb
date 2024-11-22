de4947e4e4bdbb87501e86339c4f3335744cbe88
Untung <u.untung0029@gmail.com>
2024-11-22 16:20:40 +0800

    Modifier group: validation for selection_required_max

--- app/models/modifier_group.rb
+++ app/models/modifier_group.rb
@@ -5,4 +5,5 @@ class ModifierGroup < ApplicationRecord
     has_many :modifiers
 
     validates :selection_required_min, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :selection_required_max, comparison: { greater_than_or_equal_to: :selection_required_min }, if: -> { selection_required_max.present? }
 end
