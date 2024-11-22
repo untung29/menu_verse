054e5fef09d3eece3c8fc727d38d5ef72210999a
Untung <u.untung0029@gmail.com>
2024-11-22 16:23:42 +0800

    Validations for price override and display order

--- app/models/modifier.rb
+++ app/models/modifier.rb
@@ -3,4 +3,6 @@ class Modifier < ApplicationRecord
   belongs_to :modifier_group
 
   validates :default_quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :price_override, numericality: { greater_than_or_equal_to: 0 }
+  validates :display_order, numericality: { greater_than_or_equal_to: 0 }
 end
