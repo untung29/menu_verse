e4748ff9cb9392103583a4733b458f8f86ae06e6
Untung <u.untung0029@gmail.com>
2024-11-22 16:07:05 +0800

    Validations for states only active inactive and draft

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,8 @@ class Menu < ApplicationRecord
     has_many :menu_sections, dependent: :restrict_with_error
     has_many :sections, through: :menu_sections
 
+    STATES = %w[active inactive draft].freeze
+
     validates :end_date, comparison: { greater_than: :start_date }, if: -> { end_date.present? }
+    validates :state, inclusion: { in: STATES }
 end
