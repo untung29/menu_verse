3f2eea4b436267ccb0cfd3c6a6e9f5a2d1b85d6a
Untung <u.untung0029@gmail.com>
2024-11-22 16:22:40 +0800

    Default quantity numericality validation

--- app/models/modifier.rb
+++ app/models/modifier.rb
@@ -1,4 +1,6 @@
 class Modifier < ApplicationRecord
   belongs_to :item, class_name: "Component"
   belongs_to :modifier_group
+
+  validates :default_quantity, numericality: { greater_than_or_equal_to: 0 }
 end
