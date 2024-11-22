e4577f3613afee9c0bc121f626c0f2c90da35456
Untung <u.untung0029@gmail.com>
2024-11-22 16:03:58 +0800

    Menu validation: End date should not be less than or equal to start date

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,6 @@
 class Menu < ApplicationRecord
     has_many :menu_sections, dependent: :restrict_with_error
     has_many :sections, through: :menu_sections
+
+    validates :end_date, comparison: { greater_than: :start_date }, if: -> { end_date.present? }
 end
