d63ca305fc42cfa6569d16e85304b664e671ffb8
Untung <u.untung0029@gmail.com>
2024-11-29 18:53:53 +0800

    Add origin to the deployed FE

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173", "https://menu-verse-fe.vercel.app/"
     resource "*", # Allows access to all resources
       headers: :any,
       methods: [ :get, :post, :patch, :put, :delete, :options, :head ]
